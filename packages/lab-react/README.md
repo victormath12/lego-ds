758d6147fab8be971bdaf9454e6e5eded56eed3f
Victor Caetano <victorsax12@gmail.com>
2020-06-02 23:16:04 -0300

    feat(lab-react): creating a new package to test web components in React Next App

---
+++ packages/lab-react/README.md
@@ -0,0 +1,11 @@
+# `lab-react`
+
+> TODO: description
+
+## Usage
+
+```
+const labReact = require('lab-react');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/lab-react/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello Lego.ds</h1>
+      <lego-button></lego-button>
+    </>
+  )
+}
