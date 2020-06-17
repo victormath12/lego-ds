96dfd91b0ff5a5d5b4527315ead388e294c145e8
Victor Caetano <victorsax12@gmail.com>
2020-06-16 23:25:44 -0300

    refactor(root): adjusting postbuild of react package

---
+++ packages/react/postbuild.js
@@ -0,0 +1,19 @@
+const { resolve } = require('path');
+const { writeFileSync } = require('fs');
+const [, , buildFolder] = process.argv;
+
+function postbuild(buildFolder) {
+  const packageFile = require(resolve('./package.json'));
+  const { publishConfig, scripts, main, ...jsonFile } = packageFile;
+
+  Object.assign(jsonFile, {
+    main: "./index.js",
+    module: "./index.js",
+    types: "./index.d.ts",
+    private: false,
+  });
+
+  writeFileSync(`${buildFolder}/package.json`, JSON.stringify(jsonFile));
+}
+
+postbuild(buildFolder);
